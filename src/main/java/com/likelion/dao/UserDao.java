9e122dc82c5c206fc91976b46c0e2d4892b6695f
yejinstar <gyj5586@gmail.com>
2022-10-24 14:43:50 +0900

    JdbcContext

--- src/main/java/com/likelion/dao/UserDao.java
+++ src/main/java/com/likelion/dao/UserDao.java
@@ -3,46 +3,64 @@ package com.likelion.dao;
 import com.likelion.domain.User;
 import org.springframework.dao.EmptyResultDataAccessException;
 
+import javax.sql.DataSource;
 import java.sql.*;
-import java.util.EmptyStackException;
 
 public class UserDao {
     private ConnectionMaker connectionMaker;
 
-    public UserDao() {
-        this.connectionMaker = new LocalConnectionMaker();
-    }
-    public UserDao(ConnectionMaker connectionMaker) {
-        this.connectionMaker = connectionMaker;
+    private final DataSource dataSource;
+    private final JdbcContext jdbcContext;
+
+    public UserDao(DataSource dataSource){
+        this.dataSource = dataSource;
+        this.jdbcContext = new JdbcContext(dataSource);
     }
 
-    public void jdbcContextWithStatementStrategy(StatementStrategy stmt) throws SQLException {
-        Connection c = null;
-        PreparedStatement ps = null;
-        try {
-            c = connectionMaker.makeConnection();
-            ps = stmt.makePreparedStatement(c);
-            ps.executeUpdate();
-        } catch (SQLException e) {
-            throw e;
-        } finally {
-            if (ps != null) {
-                try {
-                    ps.close();
-                } catch (SQLException e) {
-                }
-            }
-            if (c != null) {
-                try {
-                    c.close();
-                } catch (SQLException e) {
-                }
-            }
-        }
-    }
+//    public void jdbcContextWithStatementStrategy(StatementStrategy stmt) throws SQLException {
+//        Connection c = null;
+//        PreparedStatement ps = null;
+//        try {
+//            c = dataSource.getConnection();
+//            ps = stmt.makePreparedStatement(c);
+//            ps.executeUpdate();
+//        } catch (SQLException e) {
+//            throw e;
+//        } finally {
+//            if (ps != null) {
+//                try {
+//                    ps.close();
+//                } catch (SQLException e) {
+//                }
+//            }
+//            if (c != null) {
+//                try {
+//                    c.close();
+//                } catch (SQLException e) {
+//                }
+//            }
+//        }
+//    }
 
     public void deleteAll() throws SQLException, ClassNotFoundException {
-        jdbcContextWithStatementStrategy(new DeleteAllStrategy());
+//        StatementStrategy deleteAllStrategy = new StatementStrategy() {
+//            @Override
+//            public PreparedStatement makePreparedStatement(Connection connection) throws SQLException {
+//                return connection.prepareStatement("delete from users");
+//            }
+//        };
+//        jdbcContextWithStatementStrategy(new StatementStrategy() {
+//            @Override
+//            public PreparedStatement makePreparedStatement(Connection connection) throws SQLException {
+//                return connection.prepareStatement("delete from users");
+//            }
+//        });
+        jdbcContext.workWithStatementStrategy(new StatementStrategy() {
+            @Override
+            public PreparedStatement makePreparedStatement(Connection connection) throws SQLException {
+                return connection.prepareStatement("delete from users");
+            }
+        });
     }
 
     public int getCount() throws SQLException, ClassNotFoundException {
@@ -59,10 +77,32 @@ public class UserDao {
         return count;
     }
 
-    public void add(User user) throws SQLException {
-        AddStrategy addStrategy = new AddStrategy(user);
-        jdbcContextWithStatementStrategy(addStrategy);
+    public void add(final User user) throws SQLException {
+//        AddStrategy addStrategy = new AddStrategy(user);
+//        jdbcContextWithStatementStrategy(addStrategy);
+        jdbcContext.workWithStatementStrategy(new StatementStrategy() {
+            @Override
+            public PreparedStatement makePreparedStatement(Connection connection) throws SQLException {
+                PreparedStatement pstmt = null;
+                pstmt = connection.prepareStatement("INSERT INTO users(id, name, password) VALUES(?,?,?);");
+                pstmt.setString(1, user.getId());
+                pstmt.setString(2, user.getName());
+                pstmt.setString(3, user.getPassword());
+                return pstmt;
+            }
 
+        });
+//        jdbcContextWithStatementStrategy(new StatementStrategy() {
+//            @Override
+//            public PreparedStatement makePreparedStatement(Connection connection) throws SQLException {
+//                PreparedStatement pstmt = null;
+//                pstmt = connection.prepareStatement("INSERT INTO users(id, name, password) VALUES(?,?,?);");
+//                pstmt.setString(1, user.getId());
+//                pstmt.setString(2, user.getName());
+//                pstmt.setString(3, user.getPassword());
+//                return pstmt;
+//            }
+//        });
     }
 
     public User findById(String id) {
@@ -91,8 +131,8 @@ public class UserDao {
     }
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        UserDao userDao = new UserDao();
-        userDao.add(new User("03", "Ruru", "password"));
+//        UserDao userDao = new UserDao();
+//        userDao.add(new User("03", "Ruru", "password"));
         //User user = userDao.get("01");
         //System.out.println(user.getName());
     }
--- src/main/java/com/likelion/dao/UserDaoFactory.java
+++ src/main/java/com/likelion/dao/UserDaoFactory.java
@@ -2,19 +2,45 @@ package com.likelion.dao;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Map;
 
 @Configuration
 public class UserDaoFactory {
+
     @Bean
-    public UserDao awsUserDao(){
-        AwsConnectionMaker awsConnectionMaker = new AwsConnectionMaker();
-        UserDao userDao = new UserDao(awsConnectionMaker);
-        return userDao;
+    UserDao awsUserDao() {
+        return new UserDao(awsDataSource());
     }
+
     @Bean
-    public UserDao localUserDao(){
-        LocalConnectionMaker localConnectionMaker = new LocalConnectionMaker();
-        UserDao userDao = new UserDao(localConnectionMaker);
-        return userDao;
+    UserDao localUserDao() {
+        return new UserDao(localDataSource());
+    }
+
+    @Bean
+    DataSource awsDataSource() {
+        Map<String, String> env = System.getenv();
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+        dataSource.setDriverClass(com.mysql.jdbc.Driver.class);
+        dataSource.setUrl(env.get("DB_HOST"));
+        dataSource.setUsername(env.get("DB_USER"));
+        dataSource.setPassword(env.get("DB_PASSWORD"));
+        return dataSource;
+    }
+
+    @Bean
+    DataSource localDataSource() {
+        Map<String, String> env = System.getenv();
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+        dataSource.setDriverClass(com.mysql.jdbc.Driver.class);
+        dataSource.setUrl(env.get("DB_HOST"));
+        dataSource.setUsername(env.get("DB_USER"));
+        dataSource.setPassword(env.get("DB_PASSWORD"));
+        return dataSource;
     }
 }
